6267e69e6f474c05a061e5d5b0d6ff998d176d1e
Hossein <qashqaeii.ps4@gmail.com>
2023-07-13 00:18:54 +0330

    first

---
+++ resume.py
@@ -0,0 +1,84 @@
+import streamlit as st
+st.markdown("""
+<style>
+.css-cio0dv.e1g8pov61
+{
+    visibility: hidden;
+}
+</style>
+""",unsafe_allow_html=True)
+st.markdown("""
+<style>
+.css-eh5xgm.e1ewe7hr3
+{
+    visibility: hidden;
+}
+</style>
+""",unsafe_allow_html=True)
+st.markdown(
+    """
+<style>
+a:hover
+{text-shadow:0px 10px 10px;-moz-text-shadow:0px 3px 3px;-o-text-shadow:30px 30px 30px;-ms-text-shadow:0px 3px 3px;-webkit-text-shadow:30px 30px 30px;-khtml-text-shadow:30px 30px 30px;}
+</style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown("""
+<style type="text/css">
+body{
+ direction:rtl;
+}
+</style>
+""",unsafe_allow_html=True)
+
+
+col1, col2,col3 = st.columns([1,2,3])
+col1.image("profile.png", width=240)
+col3.markdown("### حسین قشقایی")
+
+col3.write("""
+دانشجوی ارشد تکنولوژی نرم افزار  👨‍🎓 عضو هیئت علمی دانشگاه بروجرد 👨‍🏫
+
+مسلط به زبان برنامه نویسی پایتون 🐍
+
+متخصص در ساخت وب اپلیکشن 👨‍💻
+
+متخصص وب اسکرپینگ 🕷 🕸
+""")
+
+st.markdown("---")
+
+linkdin,insta,yt, telg, github,gmail = st.columns([2,2,2,2,2,2])
+insta.image("instagram.png", width=50)
+insta.markdown("[اینستاگرام](https://www.instagram.com/mr.ghashghaeii/)")
+yt.image("youtube.png", width=50)
+yt.markdown("[یوتوب](https://www.youtube.com/@Automatic-show)")
+telg.image("telegram.png", width=50)
+telg.markdown("[تلگرام](https://t.me/@hosseinQashqaeii)")
+github.image("github.png", width=50)
+github.markdown("[گیت هاب](https://github.com/qashqaeii)")
+gmail.image("Gmail.png", width=50)
+gmail.markdown("[ایمیل](qashqaeii.ps4@gmail.com)")
+linkdin.image("linkedin.png", width=50)
+linkdin.markdown("[لینکدین](https://www.linkedin.com/in/qashqaei-ps4-9232b9283/)")
+st.empty()
+
+st.empty()
+st.markdown("-------")
+st.markdown("### 🤝 خدمات")
+
+st.markdown("""
+- [x]  طراحی انواع سایت های وردپرسی شامل :**خدماتی**، **شخصی**، **فروشگاهی** و غیره💻
+- [x] طراحی وب اپلیکیشن های متفاوت، بنا به درخواست و نیاز شما  👨‍🎨️
+- [X] استخراج انواع داده ، از جمله اصلاعات **بورس** ، بازارهای **ارز دیجیتال**، استخراج اطلاعات از **شبکه های اجتماعی** 📊
+- [X] آموزش 0 تا 100 **پایتون** ، در ساخت وب اپلیکیشن جذاب و کاربری
+- [X] ساخت انواع **وب اپلیکیشن** تولید محتوا (با زدن یک دکمه در هر زمینه ای، بروز محتوا تولید کنید)
+""")
+st.markdown("----")
+st.markdown("""
+""",unsafe_allow_html=True)
+st.markdown("""
+<div style="display:none"><h1></h1></div><link rel="stylesheet"href="http://dl.codebazan.ir/uploads/1461845194.css"/><div class="color-background"></div>
+<div class="azad"></div><div class="azad2"></div><div class="azad3"></div><div class="azad4"></div><div class="azad5"></div><div class="azad6"></div><div class="azad7"></div><div class="azad8"></div><div class="azad9"></div><div class="azad10"></div>
+""",unsafe_allow_html=True)
